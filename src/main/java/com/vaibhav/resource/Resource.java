42a44665034e385d9ad29bf1b3832aec8170bd79
Vaibhav <vaibhav302000@gmail.com>
2022-03-16 08:17:30 +0530

    refactoring

--- src/main/java/com/vaibhav/resource/Resource.java
+++ src/main/java/com/vaibhav/resource/Resource.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+
+@CrossOrigin()
 public interface Resource<T> {
 	@GetMapping
 	ResponseEntity<Collection<T>> findAll();
